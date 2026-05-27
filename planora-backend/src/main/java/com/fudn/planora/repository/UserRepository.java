6780c0a4aa4b9f0d6c8de0c071279f27fce7a452
VuxDucGiang <giangducx58@gmail.com>
2026-05-27 22:14:21 +0700

    feat: initialize base user and role entities with repository setup and gitignore configuration

---
+++ planora-backend/src/main/java/com/fudn/planora/repository/UserRepository.java
@@ -0,0 +1,16 @@
+//package com.fudn.planora.repository;
+//
+//import com.fudn.planora.entity.User;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.EntityGraph;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface UserRepository extend JpaRepository<User, Long> {
+//    Optional<User>
+//}
